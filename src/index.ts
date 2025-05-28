1df6f6df9ab52f7484c7f744cbe0755581400812
Vincent de Haan <vincentdehaan@gmail.com>
2025-05-28 23:55:28 +0200

    First draft

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import { PackageJson as PJ } from 'type-fest';
+import packageJson from 'package-json';
+
+const pathArg = process.argv[2];
+const packageJsonPath = path.resolve(pathArg);
+const pj = JSON.parse(readFileSync(packageJsonPath, 'utf-8')) as PJ;
+
+console.log('Name: ', pj.name);
+console.log('Number of dependencies: ', Object.keys(pj.dependencies ?? {}).length);
+
+Object.keys(pj.dependencies ?? {}).forEach(async (packageName) => {
+  let pj = await packageJson(packageName, {fullMetadata: true});
+  let own_types = pj.types
+  console.log("%s: own_types=%s", packageName, own_types != undefined);
+});
