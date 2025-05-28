2dc95957b429f75675b40406bd2ad2dc44c1bc81
Vincent de Haan <vincentdehaan@gmail.com>
2025-05-29 00:38:21 +0200

    Minor changes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import { PackageJson as PJ } from 'type-fest';
 import packageJson from 'package-json';
+import { styleText } from 'node:util';
 
 const pathArg = process.argv[2];
 const packageJsonPath = path.resolve(pathArg);
@@ -19,17 +20,16 @@ deps.concat(devDeps).forEach(async (packageName) => {
     let own_types = pj.types != undefined
     let at_types: boolean
     if(!own_types) {
+      at_types = true;
       try {
         let at = await packageJson("@types/" + packageName);
       } catch (e) {
         at_types = false;
-      } finally {
-        at_types = true;
       }
     } else {
       at_types = false
     }
-    console.log("%s %s: own_types=%s, @types=%s",!(own_types || at_types) ? "\x1b[31m" : "", packageName, own_types, at_types);
+    console.log(styleText((own_types || at_types) ? "green" : "red" ,"%s %s: own_types=%s, @types=%s"), packageName, own_types, at_types);
   } catch (e) {
     console.log("%s: cannot find dependency on npm", packageName);
   }
