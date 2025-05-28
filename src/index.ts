d465b4b00cd041d4d073cffbf75d816365ff43eb
Vincent de Haan <vincentdehaan@gmail.com>
2025-05-29 00:03:50 +0200

    Handle errors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ console.log('Name: ', pj.name);
 console.log('Number of dependencies: ', Object.keys(pj.dependencies ?? {}).length);
 
 Object.keys(pj.dependencies ?? {}).forEach(async (packageName) => {
-  let pj = await packageJson(packageName, {fullMetadata: true});
-  let own_types = pj.types
-  console.log("%s: own_types=%s", packageName, own_types != undefined);
+  try {
+    let pj = await packageJson(packageName, {fullMetadata: true});
+    let own_types = pj.types
+    console.log("%s: own_types=%s", packageName, own_types != undefined);
+  } catch (e) {
+    console.log("%s: cannot find dependency on npm", packageName);
+  }
 });
